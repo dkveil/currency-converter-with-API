679f65ee910f0237b51f2ea1b43493cbf9a7d5ab
Damian Kądziela <dkkquality@gmail.com>
2022-08-06 03:24:55 +0200

    axios added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
+import axios from 'axios'
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    const [data, setData] = useState({})
+
+    const url = 'https://currencyscoop.p.rapidapi.com/latest'
+    const token = 'f53bbfcd6emsh53cb2a99210ef13p1d0b36jsn617ef6bebbc5'
+    const host = 'currencyscoop.p.rapidapi.com'
+
+
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: url,
+            headers: {
+                'X-RapidAPI-Key': token,
+                'X-RapidAPI-Host': host
+            }
+        })
+        .then(
+            res => {
+                setData(res.data.response)
+            }
+        )
+        .catch(
+            error => console.log(error)
+        )
+
+    })
+
+    return (
+        <div className="App">
+            <div>
+
+            </div>
+        </div>
   );
 }
 
